ad9733ac6500c72de3697aa733a5b20181909c42
Shuma Shinozaki <shu.shinozaki@gmail.com>
2019-12-21 01:04:59 +0900

    ng generate @angular/material:table table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,16 @@ import { MaterialModule } from './material/material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { TableComponent } from './table/table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
@@ -19,6 +24,9 @@ import { NavComponent } from './nav/nav.component';
     HttpClientModule,
     MaterialModule,
     AppRoutingModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
