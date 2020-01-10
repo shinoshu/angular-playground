b29d5f8ae3d9e9cc480598ad68700dbc52e8dd1c
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-01-10 23:48:49 +0900

    update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,6 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { TableComponent } from './table/table.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatCardModule } from '@angular/material/card';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [
@@ -30,12 +24,6 @@ import { LayoutModule } from '@angular/cdk/layout';
     HttpClientModule,
     MaterialModule,
     AppRoutingModule,
-    MatGridListModule,
-    MatCardModule,
-    MatMenuModule,
-    MatIconModule,
-    MatButtonModule,
-    LayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
