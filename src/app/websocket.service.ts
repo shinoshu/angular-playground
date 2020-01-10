5b09d75b6a52db99532c58fa725499e41521fb0d
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-01-10 23:21:32 +0900

    update websocket.service.ts

--- src/app/websocket.service.ts
+++ src/app/websocket.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { webSocket } from 'rxjs/webSocket';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebsocketService {
+  subject = webSocket('ws://localhost:8081');
 
-  constructor() { }
+  constructor() {
+    this.subject.subscribe(
+      msg => console.log('message received: ' + msg), // Called whenever there is a message from the server.
+      err => console.log(err), // Called if at any point WebSocket API signals some kind of error.
+      () => console.log('complete') // Called when connection is closed (for whatever reason).
+    );
+  }
 }
