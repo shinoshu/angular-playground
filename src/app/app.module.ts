3a50bb89aa7e0fc593f502299d89b819b3b920f6
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-01-10 23:48:20 +0900

    ng generate @angular/material:dashboard dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { TableComponent } from './table/table.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    TableComponent
+    TableComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
@@ -22,6 +30,12 @@ import { TableComponent } from './table/table.component';
     HttpClientModule,
     MaterialModule,
     AppRoutingModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    LayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
