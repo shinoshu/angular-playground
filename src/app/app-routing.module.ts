690e074bdcfe40425210928beaf4ebfc19896102
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-01-10 23:49:56 +0900

    update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TableComponent } from './table/table.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: 'table', component: TableComponent }
+  { path: 'table', component: TableComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
