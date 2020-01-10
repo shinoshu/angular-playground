fc35e26efcff535f52d5b797cbba3671100b8f52
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-01-11 00:33:17 +0900

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { TableComponent } from './table/table.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { WorkComponent } from './work/work.component';
 
 const routes: Routes = [
   { path: 'table', component: TableComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'work', component: WorkComponent },
 ];
 
 @NgModule({
