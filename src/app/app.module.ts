16b9512fae56965ceaad93124e8bc473d18b6c46
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-01-10 23:22:43 +0900

    update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,10 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { MaterialModule } from './material/material.module';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { TableComponent } from './table/table.component';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
@@ -24,9 +22,6 @@ import { MatSortModule } from '@angular/material/sort';
     HttpClientModule,
     MaterialModule,
     AppRoutingModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
