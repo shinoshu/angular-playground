4565237a1b9da5222d542074f53d055ffdb94ded
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-01-26 19:07:37 +0900

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { TableComponent } from './table/table.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { WorkComponent } from './work/work.component';
+import { PixiComponent } from './pixi/pixi.component';
 
 const routes: Routes = [
   { path: 'table', component: TableComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'work', component: WorkComponent },
+  { path: 'pixi', component: PixiComponent },
 ];
 
 @NgModule({
