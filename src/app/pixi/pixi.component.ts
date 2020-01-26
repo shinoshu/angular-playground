578217f1f76749a437294619fd327db7533c42f9
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-01-26 19:40:08 +0900

    add pixi.js

--- src/app/pixi/pixi.component.ts
+++ src/app/pixi/pixi.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+import * as PIXI from 'pixi.js';
 
 @Component({
   selector: 'app-pixi',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pixi.component.css']
 })
 export class PixiComponent implements OnInit {
+  @ViewChild('pixi', { static: true }) pixi: ElementRef;
+
+  app: PIXI.Application;
 
   constructor() { }
 
   ngOnInit() {
+    this.app = new PIXI.Application({ width: 256, height: 256 });
+    this.pixi.nativeElement.appendChild(this.app.view);
   }
-
 }
